13e140141388c6388f4a08a8fa267cbf4f5c6477
nick-mehta <59080769+nick-mehta@users.noreply.github.com>
2020-03-19 22:17:10 +0530

    Create 1326 A.cpp

---
+++ 1326 A.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int t;
+    cin>>t;
+    int n;
+    for(int i=0;i<t;i++){
+        cin>>n;
+      
+        if(n==1){
+            cout<<"-1"<<endl;
+        }
+        else if(n==2){
+            cout<<"57"<<endl;
+        }
+        else{
+           
+             cout<<29;
+            for(int j=2;j<n;j++){
+               cout<<9;
+                   
+                    
+                
+            }
+           cout<<endl;
+            
+        }
+        
+    }
+}
